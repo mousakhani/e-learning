ef9e8a32d8367a2c63acc52007bfd38c94756947
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-29 15:55:40 +0330

    courses.views: CourseModuleUpdateView created

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic.base import TemplateResponseMixin
 
+from courses.forms import ModuleFormSet
 from courses.models import Course
 
 
@@ -43,3 +47,26 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseEditMixin, DeleteView):
 	template_name = 'courses/manage/course/delete.html'
 	permission_required = 'courses.delete_course'
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+	template_name = 'courses/manage/course/formset.html'
+	course = None
+
+	def get_formset(self, data=None):
+		return ModuleFormSet(instance=self.course, data=data)
+
+	def dispatch(self, request, pk):
+		self.course = get_object_or_404(Course, id=pk, owner=request.user)
+		return super(CourseModuleUpdateView, self).dispath()
+
+	def get(self, request, *args, **kwargs):
+		formset = self.get_formset()
+		return self.render_to_response({'course': self.course, 'formset': formset})
+
+	def post(self, request, *args, **kwargs):
+		formset = self.get_formset(data=request.POST)
+		if formset.is_valid():
+			formset.save()
+			return redirect('courses:manage_course_list')
+		return self.render_to_response({'course': self.course, 'fromser': formset})
