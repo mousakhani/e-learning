052a82a8c8bd69377e45d42e7a2104aff28dc43f
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-29 23:04:38 +0330

    api.views: SubjectListView, SubjectDetailView and CourserEnrollView added

---
+++ courses/api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from courses.api.serializers import SubjectSerializer
+from courses.models import Subject, Course
+
+
+class SubjectListView(generics.ListAPIView):
+	queryset = Subject.objects.all()
+	serializer_class = SubjectSerializer
+
+
+class SubjectDetailView(generics.RetrieveAPIView):
+	queryset = Subject.objects.all()
+	serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+	authentication_classes = (BasicAuthentication,)
+	permission_classes = (IsAuthenticated,)
+
+	def post(self, request, pk, format=None):
+		course = get_object_or_404(Course, id=pk)
+		return Response({'enrolled': True})
