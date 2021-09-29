d09c756064c6217666dbaabeecbc6fcd4b4d5998
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-29 15:55:10 +0330

    course.forms: FormSet defined

---
+++ courses/forms.py
@@ -0,0 +1,5 @@
+from django.forms.models import inlineformset_factory
+
+from courses.models import Module, Course
+
+ModuleFormSet = inlineformset_factory(Course, Module, fields=['title', 'description'], extra=2, can_delete=True)
