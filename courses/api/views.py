ca18d821455cf24f6a9ccd98fd113f358b79a167
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-30 17:22:19 +0330

    use action decorator at CourseViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -25,9 +26,18 @@ class CourseEnrollView(APIView):
 
 	def post(self, request, pk, format=None):
 		course = get_object_or_404(Course, id=pk)
+		course.students.add(request.user)
 		return Response({'enrolled': True})
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
 	queryset = Course.objects.all()
 	serializer_class = CourseSerializer
+
+	@action(detail=True, methods=['post', ], authentication_classes=[BasicAuthentication, ],
+	        permission_classes=[IsAuthenticated, ], )
+	def enroll(self, request, *args, **kwargs):
+		course = self.get_object()
+		course.students.add(request.user)
+		return Response({'enrolled': True})
+
