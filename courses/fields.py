837e21fce9ac72ba4fdba24763f93ac0fba6628b
khodemousa@gmail.com <khodemousa@gmail.com>
2021-10-01 09:39:02 +0330

    fields.py: OrderField fixed

--- courses/fields.py
+++ courses/fields.py
@@ -8,6 +8,9 @@ class OrderField(models.PositiveIntegerField):
 		super(OrderField, self).__init__(*args, **kwargs)
 
 	def pre_save(self, model_instance, add):
+		# model_instance is the instance this field belongs and
+		# add is whether the instance is being saved to the database for the first time.
+		# add False to update field
 		# self.attname refers to attribute name given to the field in the model.
 		if getattr(model_instance, self.attname) is None:
 			# not current value
@@ -20,7 +23,7 @@ class OrderField(models.PositiveIntegerField):
 					qs = qs.filter(**query)
 				# get the order of the last item
 				last_item = qs.latest(self.attname)
-				value = last_item.order + 1
+				value = getattr(last_item, self.attname) + 1
 			except ObjectDoesNotExist:
 				value = 0
 				setattr(model_instance, self.attname, value)
